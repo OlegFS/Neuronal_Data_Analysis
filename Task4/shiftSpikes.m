f793d461c81113156f274d87ef513cc0a7ad84af
catetrai <caterina@macbook-3.local>
2016-05-08 22:49:01 +0200

    Shift spikes function OK

--- Task4/shiftSpikes.m
+++ Task4/shiftSpikes.m
@@ -1,13 +1,16 @@
-function [shift, spikesShifted] = shiftSpikes(data)
+function [shifts, spikesShifted] = shiftSpikes(data)
 % *** function [shift, spikesWithLag] = shiftSpikes(data)
-%  Outputs the average lag (in samples) between measured spikes and
-%  calcium trace across all cells of the dataset.
-%  Computes the cross-correlation of spikes and calcium sequence, and
-%  estimates the shift as the lag with maximum value of cross-correlation.
-%    Input: data (struct).
+%  Estimates for each cell the lag (in samples) between measured spikes
+%  and calcium trace.
+%  Computes the cross-correlation of spikes and calcium time series
+%  of each cell, and estimates the optimal lag as the lag with
+%  maximum value of cross-correlation.
+%  Applies the shift computed for each cell to the cell's spike time
+%  series.
+%    Input: data (struct)
 %    Outputs:
-%      - estimated shift (average for all cells)
-%      - cell array of new shifted spike sequences for each cell.
+%      - 10x1 vector of estimated shifts for each cell
+%      - 1x10 cell array of shifted spike sequences for each cell
 
 data = struct2cell(data);
 data = reshape(data,[3,10]);
@@ -17,18 +20,17 @@ nCells = 10;
 maxlag = 10;  % max lag (in samples) for xcorr
 crosscorr = zeros(2*maxlag+1, nCells);  % preallocate
 lags = zeros(nCells, 2*maxlag+1);
-maxshift = zeros(nCells,1);
+shifts = zeros(nCells,1);
 
 for i=1:nCells  % for all cells
     % compute cross-correlation
     [crosscorr(:,i),lags(i,:)] = xcorr(calcium{i},spikes{i},maxlag);
-    maxshift = lags(i, find(crosscorr(:,i)==max(crosscorr(:,i))));
+    shifts(i) = lags(i, find(crosscorr(:,i)==max(crosscorr(:,i))));
 end
-shift = mean(maxshift);
 
 % implement estimated shift in the spikes sequence
 for i=1:nCells
-    spikesShifted{i} = circshift(spikes{i},shift);
+    spikesShifted{i} = circshift(spikes{i},shifts(i));
     % set first sample to zero (it is an artifact of circshift function)
     spikesShifted{i}(1) = 0;
 end
