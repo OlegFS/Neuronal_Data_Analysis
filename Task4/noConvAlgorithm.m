ac07e0d0c02cd5ec5adfc37b8a76b89610462ddc
OlegFS <o.vinogradov.92@gmail.com>
2016-05-09 11:15:00 +0200

    No Conv

--- Task4/noConvAlgorithm.m
+++ Task4/noConvAlgorithm.m
@@ -1,27 +1,28 @@
 
-[shifts, spikesShifted] = shiftSpikes(data);
+
+
+%[shifts, spikesShifted] = shiftSpikes(data);
 i=1;
-x = data(i).GalvoTraces(:);
+%x = data(i).GalvoTraces(:);
 %y = data(cells(i)).SpikeTraces(:);
-y = spikesShifted{i};
-fs = data(i).fps;
+%y = spikesShifted{i};
+%fs = data(i).fps;
 xF = filterTraces(x, fs);
-[G,f] = spikeExtraction(xF);
-[Locs,correct] = corrResp2(xF,y);
+[f,xLocks] = spikeExtraction(xF);
+%[Locs,correct] = corrResp2(xF,y);
 
 %SVMStruct = svmtrain(f',correct,'kernel_function','rbf')
 for i=1:length(f)
 ClassSpikes(i) = svmclassify(SVMStruct,f(:,i)');
 end
-
-[~,xLocks] = findpeaks(double(xF));
+%[~,xLocks] = findpeaks(double(xF));
 spikeFlag = zeros(length(xF),1);
 spikeFlag(xLocks(logical(ClassSpikes)))=1;
 heightExtimate = predictHeight(xF(xLocks(logical(ClassSpikes))),spikeFlag);
 % recontruct the data
 spikeRate = zeros(length(y),1);
-spikeRate(xLocks(logical(ClassSpikes))) = heightExtimate;
-corr(spikeRate,y)
+spikeRate(spikeFlag) = heightExtimate;
+%corr(spikeRate,y)
 
 
 
