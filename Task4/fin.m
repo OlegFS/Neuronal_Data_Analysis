333b9a7af97da994a375e2ae1b8e889b52c7735d
OlegFS <o.vinogradov.92@gmail.com>
2016-05-11 22:40:53 +0200

    More or less working code

--- Task4/fin.m
+++ Task4/fin.m
@@ -1,5 +1,7 @@
 
 % Run to finish the stuff
+load('NDA_Data_2p_only.mat')
+load SVM_Ca
 
 for i =1:55; inferredRate = estimateRateFromCa(NDA_Data(i).GalvoTraces(:),...
         NDA_Data(i).fps,SVM);
